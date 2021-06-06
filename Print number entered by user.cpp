a78585f31cfdc7721782a89272cbb0970a0ae9e4
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-06-06 12:10:52 +0530

    Create Print number entered by user.cpp

---
+++ Print number entered by user.cpp
@@ -0,0 +1,16 @@
+//Print number entered by user..
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    
+    cout<<"Enter a positive integer: "<<endl;
+    cin>>num1;
+    
+    cout<<"Number entered by user is: "<<num1<<endl;
+    
+    return 0;
+}
