935f015f70567820e4eef82f311a732b077dea9d
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-06-07 11:34:22 +0530

    Create Add 2 nos.cpp

---
+++ Add 2 nos.cpp
@@ -0,0 +1,21 @@
+//Add 2 numbers...
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    
+    cout<<"Enter First number: "<<endl;
+    cin>>num1;
+    
+    cout<<"Enter Second number: "<<endl;
+    cin>>num2;
+    
+    sum = num1 + num2;
+    
+    cout<<"Sum is: "<<sum;
+    
+    return 0;
+}
