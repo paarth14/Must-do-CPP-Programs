4c767f9be41dfbaec3d0dea001af12caf2a6641b
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-06-10 10:17:57 +0530

    Create Size of data types.cpp

---
+++ Size of data types.cpp
@@ -0,0 +1,14 @@
+//size of int, float, char, double..
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Size of int is: "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"Size of float is: "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"Size of char is: "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"Size of double is: "<<sizeof(double)<<" bytes"<<endl;
+    
+    return 0;
+}
