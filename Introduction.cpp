9f0da24795a1a1befc44d4e6b7e5f65d6c4d4f5e
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-06-28 20:50:17 +0530

    Update and rename timepass.cpp to Introduction.cpp

---
+++ Introduction.cpp
@@ -0,0 +1,14 @@
+//Introduction...
+
+#include<iostream> //Header file which includes input and output stream
+using namespace std; //instead of writing std::cout (declare this file)
+
+int main() //main function means, entry point of cpp program
+{
+    cout<<"Hello Everyone!"<<endl; //cout -> to print, endl -> new line
+    cout<<"My name is Paarth"<<endl;
+    cout<<"I am a 3rd Year Undergrad"<<endl;
+    cout<<"I live in Udaipur"<<endl;
+        
+    return 0;
+}
