f0216ff7a562a7d22c26522e2e227e5eb0e891fe
Paarth Kothari <65506287+paarth14@users.noreply.github.com>
2021-07-05 19:01:07 +0530

    Create Binary to Decimal conversion.cpp

---
+++ Binary to Decimal conversion.cpp
@@ -0,0 +1,31 @@
+//Binary to Decimal conversion...
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unsigned long long decimal_num=0; //Final decimal number
+    string binary; //Binary number to convert
+    
+    cout<<"Enter the Binary number: "<<endl;
+    cin>>binary;
+    
+    //Next step is to reverse the binary number
+    reverse(binary.begin(), binary.end());
+    
+    //String is now reversed
+    int size = binary.size();
+    
+    for(int i=0; i<size; i++)
+    {
+        if(binary[i] == '1')
+        {
+            decimal_num += pow(2, i);
+        }
+    }
+    
+    cout<<"Decimal number is: "<<decimal_num<<endl;
+    
+    return 0;
+}
